db31a05c530dbab1eb64bfed545a9ff3faae3d69
zouker <goncalojbsousa@gmail.com>
2024-08-14 17:20:46 +0100

    navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,44 @@
+import { GithubIcon } from "@/components/svg/social-icon/github-icon";
+import { LinkedinIcon } from "@/components/svg/social-icon/linkedin-icon";
+import { WebsiteIcon } from "@/components/svg/social-icon/website-icon";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="text-white ${...props}">
+            <div className="flex justify-center items-center p-4">
+
+                <div className="flex items-center mr-12">
+                    <Image
+                        className="rounded-full mr-4"
+                        src="/photo.jpeg"
+                        alt="Gonçalo Sousa Photo"
+                        width={80}
+                        height={80}
+                        draggable={false}
+                    />
+                    <div className="">
+                        <h1 className=""><b>Gonçalo Sousa</b></h1>
+                        <p>Software Developer</p>
+                    </div>
+                </div>
+
+                <div className="flex space-x-4">
+                    <Link href="https://www.google.com" target="_blank">
+                        <WebsiteIcon />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/goncalojbsousa" target="_blank">
+                        <LinkedinIcon />
+                    </Link>
+                    <Link href="https://github.com/goncalojbsousa" target="_blank">
+                        <GithubIcon />
+                    </Link>
+
+                </div>
+
+            </div>
+
+        </nav>
+    )
+}

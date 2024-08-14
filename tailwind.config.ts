bd580b833f2da01fae00afe0adf7475db07ef34b
zouker <goncalojbsousa@gmail.com>
2024-08-14 19:42:19 +0100

    define colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        color_main: "var(--color_main)",
+        color_sec: "var(--color_sec)",
+        color_sub: "var(--color_sub)",
+        color_hover: "var(--color_hover)",
       },
     },
   },
